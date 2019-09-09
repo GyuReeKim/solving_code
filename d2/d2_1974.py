85c9896ec036ce4548de3dd6d88b37ab879ad85f
GyuReeKim <starkim96@naver.com>
2019-09-09 17:09:57 +0900

    0909

--- d2/d2_1974.py
+++ d2/d2_1974.py
@@ -1,38 +1,41 @@
-def square(arr):
+def square(sudoku):
     for i in range(3):
         for j in range(3):
-            add = 0
+            add = []
             for r in range(3):
                 for c in range(3):
-                    add += arr[i*3+r][j*3+c]
-            if add != 45:
+                    add.append(sudoku[r + i*3][c + j*3])
+            if sorted(add) != number:
                 return 0
     return 1
 
-def row(arr):
+
+def row(sudoku):
     for i in range(9):
-        add = 0
+        add = []
         for j in range(9):
-            add += arr[i][j]
-        if add != 45:
+            add.append(sudoku[i][j])
+        if sorted(add) != number:
             return 0
     return 1
 
-def col(arr):
+
+def col(sudoku):
     for i in range(9):
-        add = 0
+        add = []
         for j in range(9):
-            add += arr[j][i]
-        if add != 45:
+            add.append(sudoku[j][i])
+        if sorted(add) != number:
             return 0
     return 1
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 T = int(input())
 for tc in range(1, T+1):
     sudoku = [list(map(int, input().split())) for _ in range(9)]
 
-    result = 1
-    if square(sudoku) == 0 or row(sudoku) == 0 or col(sudoku) == 0:
-        result = 0
-
-    print('#{} {}'.format(tc, result))
+    if square(sudoku) == 1 and row(sudoku) == 1 and col(sudoku) == 1:
+        print('#{} 1'.format(tc))
+    else:
+        print('#{} 0'.format(tc))
